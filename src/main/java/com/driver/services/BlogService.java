7fafd6d5b08a3027af68e26ceae6382187ea8807
Sandhy <tiwarisandhyprakash@gmail.com>
2023-03-31 00:00:47 +0530

    third

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -19,10 +19,10 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
-        User user=userRepository1.findById(userId).get();
         Blog blog=new Blog();
         blog.setTitle(title);
         blog.setContent(content);
+        User user=userRepository1.findById(userId).get();
         blog.setUser(user);
         List<Blog>blogs=user.getBlogList();
         blogs.add(blog);

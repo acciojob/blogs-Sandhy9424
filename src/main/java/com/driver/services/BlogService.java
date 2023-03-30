733ac74e684e11ec7048edf14ed51d70a06dcdce
Sandhy <tiwarisandhyprakash@gmail.com>
2023-03-31 00:00:47 +0530

    fourth

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -7,6 +7,7 @@ import com.driver.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -19,10 +20,11 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
-        User user=userRepository1.findById(userId).get();
         Blog blog=new Blog();
         blog.setTitle(title);
         blog.setContent(content);
+        blog.setPubDate(new Date());
+        User user=userRepository1.findById(userId).get();
         blog.setUser(user);
         List<Blog>blogs=user.getBlogList();
         blogs.add(blog);
